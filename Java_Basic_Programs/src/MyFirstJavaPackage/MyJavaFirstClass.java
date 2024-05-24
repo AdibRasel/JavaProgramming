9eabe9a91813dacbe29353b78ffdf523745d1437
Rasel Hossain Adib <adibrasel.2022@gmail.com>
2024-05-24 12:57:06 +0600

    Print in JAVA. জাভা তে প্রিন্ট করা
    Print in JAVA. জাভা তে প্রিন্ট করা

--- Java_Basic_Programs/src/MyFirstJavaPackage/MyJavaFirstClass.java
+++ Java_Basic_Programs/src/MyFirstJavaPackage/MyJavaFirstClass.java
@@ -1,23 +1,27 @@
 package MyFirstJavaPackage; //======প্যাকেজের নাম
 
 
-
-
-
 public class MyJavaFirstClass { // ====== ক্লাসের নাম
-
-
 
     public static void main(String[] args) { // ======= মেইন মেথড শুরু
 
-       
+        System.out.println("println = new line print"); // println দিয়ে একটা লাইনের পরে আরেকটা লাইন শুরু হবে  
+        System.out.println("println = new line print");   
+        System.out.println("println = new line print");     
 
-        System.out.println("My First Java Run Success");  
+        System.out.print("print = print one line,"); // print দিয়ে যত লাইন লিখবো সব এক লাইনে শো হবে।     
+        System.out.print("print = print one line,");     
 
-       
+
+        System.out.print("print \t = one tab space,"); // \t ব্যাক স্লেস \t দিয়ে একটা ট্যাব স্পেস নিবে
+        System.out.print("print \t = one tab space,");      
+
+        System.out.println("print = new line \n"); // \n ব্যাক স্লেস \n দিয়ে নতুন একটা লাইন শুরু হবে
+        System.out.println("print \n = new line,"); 
+        System.out.println("print \n\n\n\n = 4 new line ");      
+
 
     } // ======== মেইন মেথড শেষ
-
 
 
 }
