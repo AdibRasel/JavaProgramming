aaa3e044f18065e552f9b101290ed38365b038e9
Rasel Hossain Adib <adibrasel.2022@gmail.com>
2024-05-24 12:37:44 +0600

    Java Basic Programs
    Java Basic Programs

---
+++ Java_Basic_Programs/src/MyFirstJavaPackage/MyJavaFirstClass.java
@@ -0,0 +1,24 @@
+package MyFirstJavaPackage; //======প্যাকেজের নাম
+
+
+
+
+
+public class MyJavaFirstClass { // ====== ক্লাসের নাম
+
+
+
+    public static void main(String[] args) { // ======= মেইন মেথড শুরু
+
+       
+
+        System.out.println("My First Java Run Success");  
+
+       
+
+    } // ======== মেইন মেথড শেষ
+
+
+
+}
+
